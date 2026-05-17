ec50a3ea6910476b61460d0209441a851392576a
saivardhanvarma <byrrajusaivardhan253@gmail.com>
2026-05-17 22:19:23 +0530

    ads file added

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,23 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+	async redirects() {
+		return [
+			{
+				source: "/:path*",
+				has: [
+					{
+						type: "host",
+						value: "www.emiwyze.online",
+					},
+				],
+
+				destination: "https://emiwyze.online/:path*",
+
+				permanent: true,
+			},
+		]
+	},
+}
 
 export default nextConfig
